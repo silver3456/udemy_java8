d7b78cb24af633d9562c0191cfb4bfb050020f93
silver3456 <avseliverov@gmail.com>
2020-12-12 16:07:08 -0800

    Add test check boxes based on gender improved with pageObject

---
+++ src/main/java/com/udemy/java/pages/TableDemoPage.java
@@ -0,0 +1,31 @@
+package com.udemy.java.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class TableDemoPage {
+    private final WebDriver driver;
+
+    private static final String URL = "https://vins-udemy.s3.amazonaws.com/java/html/java8-stream-table-1.html";
+
+    public TableDemoPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void goTo() {
+        this.driver.get(URL);
+    }
+
+    public void selectRows(String gender) {
+        this.driver.findElements(By.tagName("tr"))
+                .stream()
+                .skip(1)//skip the first row
+                .map(tr -> tr.findElements(By.tagName("td")))//find all td inside tr
+                .filter(tdList -> tdList.size() == 4)// make sure that the table has 4 td (not empty) before it proceeds
+                .filter(tdList -> tdList.get(1).getText().equalsIgnoreCase(gender))//check gender by accessing second td inside tr
+                .map(tdList -> tdList.get(3)) // list of td containing checkbox
+                .map(td -> td.findElement(By.tagName("input")))
+                .forEach(WebElement::click);
+    }
+}
--- src/test/java/com/udemy/java/test/CheckBoxMaleOrFemaleAssignment.java
+++ src/test/java/com/udemy/java/test/CheckBoxMaleOrFemaleAssignment.java
@@ -1,6 +1,7 @@
 package com.udemy.java.test;
 
 import com.google.common.util.concurrent.Uninterruptibles;
+import com.udemy.java.pages.TableDemoPage;
 import com.udemy.java.supplier.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,18 +13,16 @@ import java.util.concurrent.TimeUnit;
 
 public class CheckBoxMaleOrFemaleAssignment {
     private WebDriver driver;
-    private static final String URL = "https://vins-udemy.s3.amazonaws.com/java/html/java8-stream-table.html";
+    private TableDemoPage tableDemoPage;
 
-//    List<WebElement> checkboxes = this.driver.findElements(By.cssSelector("input[type='checkbox']"));
-//    List<WebElement> male = this.driver.findElements(By.xpath("//td[text() = 'Male']"));
-//    List<WebElement> female = this.driver.findElements(By.xpath("//td[text() = 'Female']"));
-//    List<WebElement> rows = this.driver.findElements(By.xpath("//tr"));
+    private static final String URL = "https://vins-udemy.s3.amazonaws.com/java/html/java8-stream-table-1.html";
 
 
     @BeforeTest
     @Parameters("browser")
     public void setDriver(@Optional("chrome") String browser) {
         this.driver = DriverFactory.getDriver(browser);
+        this.tableDemoPage = new TableDemoPage(driver);
     }
 
     @Test
@@ -41,12 +40,21 @@ public class CheckBoxMaleOrFemaleAssignment {
                 .stream()
                 .skip(1)//skip the first row
                 .map(tr -> tr.findElements(By.tagName("td")))//find all td inside tr
+                .filter(tdList -> tdList.size() == 4)// make sure that the table has 4 td (not empty) before it proceeds
                 .filter(tdList -> tdList.get(1).getText().equalsIgnoreCase(gender))//check gender by accessing second td inside tr
                 .map(tdList -> tdList.get(3)) // list of td containing checkbox
                 .map(td -> td.findElement(By.tagName("input")))
                 .forEach(WebElement::click);
 
         Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS); // sleep for a sec to see what's happening
+    }
+
+    @Test(dataProvider = "gender")
+    public void selectCheckBoxGenderImprovedValidation(String gender){
+        tableDemoPage.goTo();
+        tableDemoPage.selectRows(gender);
+        Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS); // sleep for a sec to see what's happening
+
     }
 
     @DataProvider(name = "gender")
