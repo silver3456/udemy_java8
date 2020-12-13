9b929da07b1842b3b5375f85fda145a2f0b1be0f
silver3456 <avseliverov@gmail.com>
2020-12-13 12:50:46 -0800

    add findFirst which is Optional

---
+++ src/test/java/TerminalOperTest.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TerminalOperTest {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("alex");
+        list.add("bob");
+        list.add("david");
+        list.add("john");
+
+        /* Usage of Optional
+
+        Optional<String> op = list.stream()
+                .filter(s -> s.length() > 3)
+                .findFirst();
+
+        if (op.isPresent())
+            System.out.println(op.get());
+
+            */
+
+        System.out.println(
+
+//                list.stream()
+//                .filter(s -> s.length() > 3)
+//                .count() // counts after some changes are made inside the list
+
+
+
+                list.stream()
+                        .filter(s -> s.length() > 3)
+                        .findFirst() //returns Optional meaning if there is no satisfied
+                                    //condition then it will not do anything
+
+        );
+    }
+}
