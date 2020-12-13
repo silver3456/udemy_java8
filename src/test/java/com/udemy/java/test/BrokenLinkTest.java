2b685e5e282c5c33ed4934e26ca8063a5fc07654
silver3456 <avseliverov@gmail.com>
2020-12-13 13:32:10 -0800

    add test to output result into testng report

--- src/test/java/com/udemy/java/test/BrokenLinkTest.java
+++ src/test/java/com/udemy/java/test/BrokenLinkTest.java
@@ -4,7 +4,11 @@ import com.udemy.java.supplier.DriverFactory;
 import com.udemy.java.util.LinkUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BrokenLinkTest {
 
@@ -17,7 +21,7 @@ public class BrokenLinkTest {
     }
 
     @Test
-    public void linkTest() {
+    public void testBrokenImages() {
         this.driver.get("https://the-internet.herokuapp.com/broken_images");
         this.driver.findElements(By.xpath("//*[@src]"))
                 .stream()
@@ -25,11 +29,48 @@ public class BrokenLinkTest {
                 .forEach(src -> {
                             System.out.println(LinkUtil.getResponseCode(src) + " :: " + src);
                         }
-
-                            );
-
+                );
 
     }
+
+    @Test
+    public void testBrokenLinks() {
+        this.driver.get("https://the-internet.herokuapp.com/broken_images");
+        this.driver.findElements(By.xpath("//*[@href]"))
+                .stream()
+                .map(e -> e.getAttribute("href"))
+                .forEach(src -> {
+                            System.out.println(LinkUtil.getResponseCode(src) + " :: " + src);
+                        }
+                );
+
+    }
+
+    @Test
+    public void testBrokenImagesImproved() {
+        this.driver.get("https://the-internet.herokuapp.com/broken_images");
+        boolean result = this.driver.findElements(By.xpath("//*[@src]"))
+                .stream()
+                .map(e -> e.getAttribute("src"))
+                .map(src -> LinkUtil.getResponseCode(src))
+                .anyMatch(rc -> rc != 200);
+
+        Assert.assertFalse(result);
+    }
+
+
+    @Test
+    public void testBrokenImagesWithStackTrace() {
+        this.driver.get("https://the-internet.herokuapp.com/broken_images");
+        List<String> list = this.driver.findElements(By.xpath("//*[@src]"))
+                .stream()
+                .map(e -> e.getAttribute("src"))
+                .filter(src -> LinkUtil.getResponseCode(src) != 200)
+                .collect(Collectors.toList());// create a new list that stores broken images
+
+        Assert.assertEquals(list.size(), 0, list.toString());
+    }
+
 
     @AfterTest
     public void quitDriver() {
