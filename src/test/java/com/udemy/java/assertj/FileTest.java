4d94857eca690a1d59459b23dfede1ef68a6e6a6
silver3456 <avseliverov@gmail.com>
2020-12-20 13:34:44 -0800

    add file assertions

---
+++ src/test/java/com/udemy/java/assertj/FileTest.java
@@ -0,0 +1,21 @@
+package com.udemy.java.assertj;
+
+import org.testng.annotations.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileTest {
+
+    @Test
+    public void fileTest() {
+
+       Path exptectedFile = Paths.get("src/test/resources/data/expected.txt");
+       Path actualFile = Paths.get("src/test/resources/data/actual.txt");
+
+
+       assertThat(actualFile).hasSameContentAs(exptectedFile);
+    }
+}
