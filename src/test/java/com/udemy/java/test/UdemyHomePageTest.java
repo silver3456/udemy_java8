ef680cbeeda156f5da68551c27f89f540971cd90
silver3456 <avseliverov@gmail.com>
2021-05-25 18:36:28 -0400

    Add hoverable test Udemy page

--- src/test/java/com/udemy/java/test/UdemyHomePageTest.java
+++ src/test/java/com/udemy/java/test/UdemyHomePageTest.java
@@ -4,13 +4,14 @@ import com.google.common.util.concurrent.Uninterruptibles;
 import com.udemy.java.supplier.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-public class HoverableDropdownTest {
+public class UdemyHomePageTest {
 
     private WebDriver driver;
     private Actions actions;
@@ -25,29 +26,31 @@ public class HoverableDropdownTest {
 
     @Test(dataProvider = "linkProvider")
     public void dropdownTest(String path) {
-        this.driver.get("https://vins-udemy.s3.amazonaws.com/java/html/drop-down.html");
+        this.driver.get("https://www.udemy.com/");
+        WebElement el = driver.findElement(By.xpath("//span[text()='Categories']"));
+        actions.moveToElement(el).perform();
+
         String[] split = path.split("=>");
         Arrays.stream(split)
                 .map(s -> s.trim())
-                .map(s -> By.linkText(s)) // String convert to by locator = find the 'by' locator
+                .map((s -> By.xpath("//div[text()='"+ s + "']")))
                 .map(by -> driver.findElement(by))// use by to find the WebElement
                 .map(elem -> actions.moveToElement(elem))
                 .forEach(a -> a.perform());
 
-        Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
 
     }
 
     @DataProvider(name = "linkProvider")
     public Object[] testData() {
         return new Object[]{
-                "Dropdown => Dropdown Link 2",
-                "Dropdown => Dropdown Link 5 => Dropdown Submenu Link 5.1",
-                "Dropdown => Dropdown Link 5 => Dropdown Submenu Link 5.4 => Dropdown Submenu Link 5.4.2",
+                "Development => Web Development => JavaScript",
+                "Business => Management => Leadership"
         };
     }
 
-        @AfterTest
+    @AfterTest
     public void quitDriver() {
         this.driver.quit();
     }
